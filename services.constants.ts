2c17b366fe533ad7fc899f5e81dbb855d68b0dca
oliveira-mauricio <113799844+oliveira-mauricio@users.noreply.github.com>
2026-04-20 18:59:51 -0300

    Add service constants for services and icons

---
+++ services.constants.ts
@@ -0,0 +1,22 @@
+// Constants for services and icons
+
+export const SERVICE_CONSTANTS = {
+    SERVICE_A: {
+        name: 'Service A',
+        icon: 'icon-service-a',
+    },
+    SERVICE_B: {
+        name: 'Service B',
+        icon: 'icon-service-b',
+    },
+    SERVICE_C: {
+        name: 'Service C',
+        icon: 'icon-service-c',
+    },
+};
+
+export const ICON_CONSTANTS = {
+    ICON_A: 'icon-a',
+    ICON_B: 'icon-b',
+    ICON_C: 'icon-c',
+};
